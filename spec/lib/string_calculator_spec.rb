0fc9439911732b58ff31b2e7d3fbc99525de3751
kalyansrinivas001 <103947682+kalyansrinivas001@users.noreply.github.com>
2024-07-15 14:34:31 +0530

    Add test case - rspec

---
+++ spec/lib/string_calculator_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+require 'string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    context 'when numbers are provided' do
+      it 'returns the sum of numbers' do
+        expect(StringCalculator.add("")).to eq(0)
+        expect(StringCalculator.add("1")).to eq(1)
+        expect(StringCalculator.add("1,2")).to eq(3)
+        expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
+      end
+    end
+
+    context 'when empty string is provided' do
+      it 'returns 0' do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+  end
+end
